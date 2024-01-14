16148c6c23bc424b427aceb3c0e6c188e2d9207b
Vandyck <vandycklai@gmail.com>
2024-01-14 16:16:23 +0800

    Reset deployment yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,10 @@ const DEV_PORT = 4321;
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // site: process.env.CI
-	// 	? 'https://laikaiyong.github.io'
-	// 	: `http://localhost:${DEV_PORT}`,
-	// base: process.env.CI ? '/Equipay/' : undefined,
+  site: process.env.CI
+		? 'https://laikaiyong.github.io'
+		: `http://localhost:${DEV_PORT}`,
+	base: process.env.CI ? '/Equipay' : undefined,
 	server: {
 		/* Dev. server only */
 		port: DEV_PORT,
